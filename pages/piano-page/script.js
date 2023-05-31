9cfb1dc734168399014fd44fb0153a01dfe993af
Miras <bking4234@gmail.com>
2023-05-31 21:25:27 +0600

    init: add button link and debug some elements

--- pages/piano-page/script.js
+++ pages/piano-page/script.js
@@ -1,22 +1,35 @@
-//добавляю событие на весь сайт, если нажата  любая клавиша(keydown) то срабатывает функция
+
 window.addEventListener('keydown', function(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //получаю то аудио у которого data-key равно на нажатую клавишу(метод keyCode) 
-    const key = document.querySelector(`.white-key[data-key="${e.keyCode}"]`);//получаю ту клавишу у которого data-key ровно на нажатую клавишу(метод keyCode), чтобы в дальнейшем анимировать
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); 
+    const key = document.querySelector(`.white-key[data-key="${e.keyCode}"]`);
 
-    if(!audio) return; //если нажатая клавиша не равно data-key из audio, то заканчивай функцию
+    if(!audio) return; 
 
-    audio.currentTime = 0; //метод currentTime удаляет задержку(звук обычно 1 секунда, метод делает его 0с чтобы быстро воспроизвести)
-    audio.play();//запускает музыку
+    audio.currentTime = 0; 
+    audio.play();
 
-    key.classList.add('active'); //добавляет класс active на клавишу (добавление анимаций)
+    key.classList.add('active'); 
     
 })
 
-//функция которая удаляет анимацию после срабатывания
 function removeTransition(e){
-    if(e.propertyName !== 'background-color') return; //если выбранный текущий элемент не равно элементу white-key c стилем background-color то заканчивай функцию
-    this.classList.remove('active'); //удаляет класс active в текущем элемента(а именно клавиша span)
+    if(e.propertyName !== 'background-color') return;
+    this.classList.remove('active'); 
 }
 
-const whiteKeys = document.querySelectorAll('.white-key'); //получаю все элементы с классом white-key
-whiteKeys.forEach(key => key.addEventListener('transitionend', removeTransition)); // для всех элементов запускаю событие(метод forEach), событие transitionend - это когда анимация заканчивается
+const whiteKeys = document.querySelectorAll('.white-key'); 
+whiteKeys.forEach(key => key.addEventListener('transitionend', removeTransition)); 
+
+
+
+const linkToMain = document.querySelector(".linkToMain");
+linkToMain.addEventListener("mouseover", handleLink);
+linkToMain.addEventListener("mouseout", removeLink);
+
+
+function handleLink(){
+    linkToMain.classList.add("linkToMain-active");
+}
+function removeLink(){
+    linkToMain.classList.remove("linkToMain-active");
+}
